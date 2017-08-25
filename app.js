8cb8465588040da496bc1a13100d34a8858dd960
Mladen <lucicmladen83@gmail.com>
2017-08-25 22:53:08 +0200

    Challenges finished.

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ GAME RULES:
 */
 
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, diceSix, endGame = 100;
 init();
+setValue();
 
 
 
@@ -20,18 +21,29 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         //1. Random number
         var dice = Math.floor(Math.random() * 6) + 1;
         //2. Display result
-        var diceDOM = document.querySelector('.dice');
+        var diceDOM = document.querySelector('.dice-' + activePlayer);
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
         //3. Update the round score IF the rolled number was NOT a 1
         if (dice > 1) {
-            //add score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+            if ((dice + diceSix) === 12) {
+                scores[activePlayer] = 0;
+                document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+                diceSix = 0;
+                nextPlayer();
+            } else {
+                diceSix = dice;
+                //add score
+                roundScore += dice;
+                document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            }
+
         } else {
             //next player
             nextPlayer();
         }
+
     }
 });
 
@@ -44,9 +56,9 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //Check if player won the game
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= endGame) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice-'+ activePlayer).style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -64,14 +76,13 @@ document.querySelector('.btn-new').addEventListener('click', init);
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
+    diceSix = 0;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
 }
 
 function init() {
@@ -79,8 +90,13 @@ function init() {
     roundScore = 0;
     activePlayer = 0;
     gamePlaying = true;
-
-    document.querySelector('.dice').style.display = 'none';
+    if (endGame < 10) {
+        endGame = 100;
+    }
+    document.querySelector('.set-peak input').value = endGame;
+    document.querySelector('.set-peak h4').textContent = "First one reaches " + endGame + " si the winner!";
+    document.querySelector('.dice-0').style.display = 'none';
+    document.querySelector('.dice-1').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -96,5 +112,35 @@ function init() {
     document.querySelector('.player-0-panel').classList.add('active');
 }
 
+
+function setValue() {
+    var inputSet = document.getElementById('high-end');
+    inputSet.addEventListener("keydown", function (event) {
+        if (event.keyCode == 13) {
+            endGame = inputSet.value;
+            document.querySelector('.set-peak h4').textContent = "First one reaches " + endGame + " si the winner!";
+            inputSet.blur();
+        }
+        if (endGame > 10) {
+            return endGame;
+        } else if (endGame === '') {
+            endGame = 100;
+            document.querySelector('.set-peak h4').textContent = "First one reaches " + endGame + " si the winner!";
+            init();
+            inputSet.placeholder = endGame;
+        } else if (endGame < 10) {
+            document.querySelector('.set-peak h4').textContent = "Set number higher than 10";
+            gamePlaying = false;
+        }
+    });
+}
+
+
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+ dice + '</em>';
+/*
+1. Dvije sestice zaredom - igrac gubi sve poene
+2. Input za postavljanje pobjedonosnog rezultata 
+3. Kockica za svakog igraca - dvije kockice
+
+*/
